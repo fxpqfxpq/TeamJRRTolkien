e33f3eae413ddf795bdd34504cd3ff6591eccc8f
Ivan Aleksandrov <vancuver@gmail.com>
2014-02-26 00:41:09 +0200

    added few options to Item.cs
    required level, stackable, etc.

--- CrystallBallGame/Item.cs
+++ CrystallBallGame/Item.cs
@@ -7,11 +7,22 @@
     {
         protected string name;
         protected int pointValue; // how many points it adds to the Creature's attributes
+        protected int requiredLevel; // required Hero level to use item
+        protected int stackable; //if item is stackable: 0 = no, 1 can stack with 1 only, 2 with 2, etc.
 
-        public Items(string name)
+        public Items(string name, int pointValue, int requiredLevel, int stackable)
         {
             this.Name = name;
+            this.PointValue = pointValue;
+            this.RequiredLevel = requiredLevel;
+            this.Stackable = stackable;
         }
+
+        public int PointValue { get; set; }
+
+        public int RequiredLevel { get; set; }
+
+        public int Stackable { get; set; }
 
         public string Name
         {
