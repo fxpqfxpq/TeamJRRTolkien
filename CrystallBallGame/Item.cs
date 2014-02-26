04c17470cb6ac15a21927ab399849de4cbc8103b
NivCanFly <rinsingwind@gmail.com>
2014-02-26 19:12:08 +0200

    Updated encapsulation in Items.cs

--- CrystallBallGame/Item.cs
+++ CrystallBallGame/Item.cs
@@ -9,20 +9,26 @@
         protected int pointValue; // how many points it adds to the Creature's attributes
         protected int requiredLevel; // required Hero level to use item
         protected int stackable; //if item is stackable: 0 = no, 1 can stack with 1 only, 2 with 2, etc.
+        protected Creature itemOwner;
 
-        public Items(string name, int pointValue, int requiredLevel, int stackable)
+        public Items(string name, int requiredLevel, int stackable)
         {
-            this.Name = name;
-            this.PointValue = pointValue;
-            this.RequiredLevel = requiredLevel;
-            this.Stackable = stackable;
+            this.name = name;
+            this.requiredLevel = requiredLevel;
+            this.stackable = stackable;
         }
 
-        public int PointValue { get; set; }
+        public int PointValue { get { return this.pointValue; } }
 
-        public int RequiredLevel { get; set; }
+        public int RequiredLevel { get { return this.requiredLevel; } }
 
-        public int Stackable { get; set; }
+        public int Stackable { get { return this.stackable; } }
+
+        public Creature ItemOwner 
+        { 
+            get { return this.itemOwner; }
+            set { this.itemOwner = value; }
+        }
 
         public string Name
         {
