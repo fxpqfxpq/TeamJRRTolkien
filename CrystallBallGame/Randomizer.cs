fc8dfcff764e04c5b5f910d688f4f966ed9d879d
Ivan Aleksandrov <vancuver@gmail.com>
2014-02-26 18:40:49 +0200

    Randomizer
    new class for random events

---
+++ CrystallBallGame/Randomizer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace CrystallBallGame
+{
+    public static class Randomizer
+    {
+        private static Random rnd = new Random();
+
+        public static Random Rnd
+        {
+            get { return rnd; }
+        }
+    }
+}
